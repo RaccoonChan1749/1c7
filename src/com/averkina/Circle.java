92ba4d3b5248446ef3931485a1a5ad6726719219
RaccoonChan1749 <zhenyaaverkina@mail.ru>
2022-04-04 09:40:37 +0300

    Initial commit

---
+++ src/com/averkina/Circle.java
@@ -0,0 +1,20 @@
+package com.averkina;
+
+import java.util.Random;
+
+public class Circle implements Shape{
+    double r;
+
+    public Circle (double range) {
+        Random random = new Random();
+        r = range * random.nextDouble();
+    }
+
+    public String getType() {
+        return "Круг (r=" + String.format("%.2f", r) + ")";
+    }
+
+    public double getArea() {
+        return Math.PI * r * r;
+    }
+}
---
+++ src/com/averkina/Rectangle.java
@@ -0,0 +1,22 @@
+package com.averkina;
+
+import java.util.Random;
+
+public class Rectangle implements Shape{
+    double a;
+    double b;
+
+    public Rectangle (double range) {
+        Random random = new Random();
+        a = range * random.nextDouble();
+        b = range * random.nextDouble();
+    }
+
+    public String getType() {
+        return "Квадрат (a=" + String.format("%.2f", a) + "; b=" + String.format("%.2f", b) + ")";
+    }
+
+    public double getArea() {
+        return a*b;
+    }
+}
